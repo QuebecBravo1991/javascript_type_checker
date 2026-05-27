20637877801e2c29d25294565c8e7b06f418a466
Quade Barnes <quade.barnes@gmail.com>
2026-05-27 14:07:19 -0600

    Add program read and parse to AST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use oxc_allocator::Allocator;
+use oxc_parser::Parser;
+use oxc_span::SourceType;
+use std::fs;
+use std::io;
+
+fn read_program(path: &str) -> Result<String, io::Error> {
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
+}
+
+fn gen_ast(source: String) {
+    let allocator = Allocator::default();
+    let source_type = SourceType::default();
+    let ret = Parser::new(&allocator, &source, source_type).parse();
+    let program = ret.program;
+
+    println!("AST:");
+    println!("{program:#?}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let source = read_program("example.js").unwrap();
+    gen_ast(source);
 }
